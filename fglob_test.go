fd4254ccd9eb1ba9e6e5c654cd4d7ddba3e5ca01
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-24 09:34:59 -0300

    file glob

---
+++ fglob_test.go
@@ -0,0 +1,42 @@
+package fileglob
+
+import (
+	"os"
+	"testing"
+	"path/filepath"
+	"github.com/franela/goblin"
+)
+
+func TestGlob(t *testing.T) {
+	pwd, _ := os.Getwd()
+	dir := filepath.Join(pwd, "test")
+	g := goblin.Goblin(t)
+
+	g.Describe("file glob", func() {
+		g.It("fglob", func() {
+			ignoreDirs := []string{".bzr", ".hg", ".git", ".idea", ".directory"}
+			filters := []string{"JPG", "JPEG"}
+			res, err := Glob(dir, filters, false, ignoreDirs)
+			g.Assert(len(res)).Equal(8)
+			g.Assert(err == nil).IsTrue()
+		})
+	})
+
+	g.Describe("file glob stream", func() {
+		g.It("stream files", func() {
+			ignoreDirs := []string{".bzr", ".hg", ".git", ".idea", ".directory"}
+			filters := []string{"JPG", "JPEG"}
+			res := make([]string, 0)
+			stream := GlobStream(dir, filters, false, ignoreDirs, 25)
+			g.Assert(stream != nil).IsTrue()
+
+			for buf := range stream {
+				res = append(res, buf...)
+			}
+			g.Assert(len(res)).Equal(8)
+		})
+	})
+
+
+
+}
